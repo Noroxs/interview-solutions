de74f3789a48bfbf98b5253b315d90c3e06aaf3d
Ramson, Thomas <t.ramson@thalia.de>
2019-02-20 18:30:59 +0100

    Initial Commit

---
+++ eNoteInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eNoteInterview
+//
+//  Created by Ramson, Thomas on 2/20/19.
+//  Copyright © 2019 Thomas Ramson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
