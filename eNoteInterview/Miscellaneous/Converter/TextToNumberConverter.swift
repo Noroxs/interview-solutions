31987be127664079c97790c95e4514913573f640
Ramson, Thomas <t.ramson@thalia.de>
2019-02-21 20:13:14 +0100

    adding struct to convert entered text to number

---
+++ eNoteInterview/Miscellaneous/Converter/TextToNumberConverter.swift
@@ -0,0 +1,36 @@
+//
+//  TextToNumberConverter.swift
+//  eNoteInterview
+//
+//  Created by Ramson, Thomas on 2/21/19.
+//  Copyright © 2019 Thomas Ramson. All rights reserved.
+//
+
+import UIKit
+
+struct TextToNumberConverter {
+    
+    let validCharacters = Constants.validNumberCharacters
+    
+    func textOnlyContainValidCharacters(_ text: String) -> Bool {
+        
+        let allowedCharacters = CharacterSet(charactersIn: validCharacters)
+        let stringCharacterSet = CharacterSet(charactersIn: text)
+        
+        return allowedCharacters.isSuperset(of: stringCharacterSet)
+    }
+    
+    func textContainsValidNumber(_ text: String) -> Bool {
+        
+        let textNumber = number(for: text)
+        let isTextNumberValid = textNumber > 0
+        
+        return isTextNumberValid
+    }
+    
+    func number(for text: String) -> Int {
+        
+        return Int(text) ?? 0
+    }
+    
+}
