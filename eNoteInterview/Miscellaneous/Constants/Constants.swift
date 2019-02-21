e3138db4b5ebcfc34175abbbfa426dd3f08fa0bc
Ramson, Thomas <t.ramson@thalia.de>
2019-02-21 20:08:51 +0100

    adding constant struct

---
+++ eNoteInterview/Miscellaneous/Constants/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  eNoteInterview
+//
+//  Created by Ramson, Thomas on 2/21/19.
+//  Copyright © 2019 Thomas Ramson. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    static let validNumberCharacters = "0123456789"
+    
+    static let valueAmountTableViewCellIdentifier = "ValueAmountTableViewCellIdentifier"
+    
+}
