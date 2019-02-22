450c295288bbc5d70cadc03b0f0b6a05f21817ba
Ramson, Thomas <t.ramson@thalia.de>
2019-02-23 00:49:41 +0100

    adding IntervalWorker to give the user faster feedback if large numbers needs to be evaluated

--- eNoteInterview/Miscellaneous/Logic/Workers.swift
+++ eNoteInterview/Miscellaneous/Logic/Workers.swift
@@ -53,8 +53,62 @@ struct Worker: Working {
     
 }
 
-struct IntervalWorker {
+struct IntervalWorker: Working {
     
     private let intervalGenerator: IntervalGenerating
+    private var analyzer: Analyzing
+    private let sorter: Sorting
+    private let mapper: Mapping
+    
+    var result: DynamicBinding<(values: [ValueEntity<Int>], finished: Bool)> = DynamicBinding((values: [], finished: false))
+    
+    init(intervalGenerator: IntervalGenerating = IntervalRandomGenerator(),
+         analyzer: Analyzing = MemoryStorageAnalyzer(),
+         sorter: Sorting = Sorter(),
+         mapper: Mapping = Mapper()) {
+        
+        self.intervalGenerator = intervalGenerator
+        self.analyzer = analyzer
+        self.sorter = sorter
+        self.mapper = mapper
+    }
+    
+    func startCreationSortingAndMapping(for amount: Int) {
+        
+        analyzer.clear()
+        
+        DispatchQueue.global(qos: .background).async {
+            let generatedNumbers = self.intervalGenerator.generateNumbers(amount: amount)
+            let mappedNumbers = self.analyzeSortAndMap(numbers: generatedNumbers)
+            self.reportResult(values: mappedNumbers, finished: self.intervalGenerator.finished)
+        }
+    }
+    
+    func performNextIteration() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let generatedNumbers = self.intervalGenerator.continue()
+            let mappedNumbers = self.analyzeSortAndMap(numbers: generatedNumbers)
+            self.reportResult(values: mappedNumbers, finished: self.intervalGenerator.finished)
+        }
+    }
+    
+    private func analyzeSortAndMap(numbers: [Int]) -> [ValueEntity<Int>] {
+        
+        let analyzedNumbers = analyzer.analyze(numbers: numbers)
+        let sortedNumbers = sorter.sort(dictionary: analyzedNumbers)
+        return mapper.mapToValueEntities(sortedNumbers: sortedNumbers)
+    }
+    
+    private func reportResult(values: [ValueEntity<Int>], finished: Bool) {
+        
+        DispatchQueue.main.async {
+            self.result.value = (values, finished)
+            
+            if finished == false {
+                self.performNextIteration()
+            }
+        }
+    }
     
 }
